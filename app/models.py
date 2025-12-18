1155335716a38df3e532f6fb5fa6f062cbce2d53
avelowmed <awelowmeed@mail.ru>
2025-12-18 17:08:06 +0300

    fix tablename

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from .database import Base
 
 
 class Note(Base):
-    __tablename__  == "notes"
+    __tablename__  = "notes"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(nullable=False)
