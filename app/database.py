1a3019b946ea6be75684c2db9db63c580267997d
avelowmed <awelowmeed@mail.ru>
2025-12-19 17:31:48 +0300

    fix dir import

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from .config import settings
+from core.config import settings
 
 
 class Base(DeclarativeBase):
