c8a223198158f560f85ca4a3eb16d92c61d34356
avelowmed <awelowmeed@mail.ru>
2025-12-19 18:12:28 +0300

    fix 2 fields in note table

--- app/models.py
+++ app/models.py
@@ -8,5 +8,5 @@ class Note(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(nullable=False)
-    created_at = Mapped[datetime] = mapped_column(default=datetime.utcnow)
-    complited = Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    complite:  Mapped[bool] = mapped_column(default=False)
